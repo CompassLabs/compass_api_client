25755f99665293a6843ad3ecd337b82465e27d2b
Github Automation <ollivanders@users.noreply.github.com>
2025-04-02 13:29:40 +0000

    update: clients

--- python_client/setup.py
+++ python_client/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "compass.api_client"
-VERSION = "1.0.140"
+VERSION = "1.0.141"
 PYTHON_REQUIRES = ">= 3.9"
 REQUIRES = [
     "urllib3 >= 2.1.0, < 3.0.0",
